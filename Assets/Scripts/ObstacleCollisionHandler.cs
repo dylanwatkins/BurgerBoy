16b51d0b4c0fd59972ce92ea14708db8929f9fb0
kparks29 <ken@awesome>
2014-09-15 15:19:45 -0700

    create welcome, and game over screens

--- Assets/Scripts/ObstacleCollisionHandler.cs
+++ Assets/Scripts/ObstacleCollisionHandler.cs
@@ -16,36 +16,40 @@ public class ObstacleCollisionHandler : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider collider) {
-		collider.gameObject.SetActive(false);
-		int lives = this.GetComponent<TruckManager>().lives;
-		if (lives > 0) {
-			this.GetComponent<TruckManager>().lives -= 1;
-			this.GetComponent<TruckManager>().burgerLives[lives-1].gameObject.SetActive(false);
+		if(collider.name != "hamburger(Clone)"){
+
+			collider.gameObject.SetActive(false);
+			int lives = this.GetComponent<TruckManager>().lives;
+			if (lives > 0) {
+				this.GetComponent<TruckManager>().lives -= 1;
+				this.GetComponent<TruckManager>().burgerLives[lives-1].gameObject.SetActive(false);
+			}
+			checkEndGame();
 		}
-		checkEndGame();
 	}
 
 	void checkEndGame(){
 		if (this.GetComponent<TruckManager>().lives == 0) {
-			Debug.Log("GAME OVER");
-			this.gameObject.SetActive(false);
-			gameOver.gameObject.SetActive(true);
+			Application.LoadLevel("GameOverScene");
+//			Debug.Log("GAME OVER");
+//			this.gameObject.SetActive(false);
+//			gameOver.gameObject.SetActive(true);
 
-			WWWForm form = new WWWForm();
-			form.AddField("initials","KAP");
-			form.AddField("score",GameObject.Find("MovementQueueManager").GetComponent<MovementHandler>().score); 
-			WWW url = new WWW("http://www.burgermonster.net/game", form);
-
-			if(url.isDone && url.error == null) {
-				gameOver.text = url.error;
-				print( "Error downloading: " + url.error );
-			} 
-			else if (url.isDone){
-				// show the highscores
-				gameOver.text = url.text;
-				print(url.text);
-			}
-			Time.timeScale = 0.0f;
+//			WWWForm form = new WWWForm();
+//			form.AddField("initials","KAP");
+//			form.AddField("score",GameObject.Find("MovementQueueManager").GetComponent<MovementHandler>().score); 
+//			WWW url = new WWW("http://www.burgermonster.net/game", form);
+//
+//			if(url.isDone && url.error == null) {
+//				gameOver.text = url.error;
+//				print( "Error downloading: " + url.error );
+//			} 
+//			else if (url.isDone){
+//				// show the highscores
+//				gameOver.text = url.text;
+//				print(url.text);
+//			}
+//			Time.timeScale = 0.0f;
 
 		}
 	}
---
+++ Assets/Scripts/WelcomeScreenEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WelcomeScreenEventHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void startGame() {
+		Application.LoadLevel("Level02");
+	}
+
+	public void exitGame() {
+		Application.Quit();
+	}
+
+	public void loadHighScores() {
+
+	}
+}
